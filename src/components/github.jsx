433cf0a2539e8981601e29e2d7a4d0d75b937a10
vinayak2882 <vinayak.patrut@gmail.com>
2022-05-21 13:02:50 +0530

    first

--- src/components/github.jsx
+++ src/components/github.jsx
@@ -1,40 +1,71 @@
+
 import axios from "axios";
 
 const {useState, useEffect}=require("react");
 
-const getGithubuser=(q="albseb511")=>{
+const getGithubuser=(q="vinayak2882", page=1)=>{
    return axios("https://api.github.com/search/users",
     {method:"GET",
      params:{
-        q:q
+        q,
+        per_page:5,
+        page
     }
 })
    
 };
 
 function Github(){
-    // const [loading, setLoading]=useState();
+    const [loading, setLoading]=useState();
     const [error, setError]=useState(false);
+    const [query, setQuery]=useState("masai")
     const [data, setData]=useState([]);
+    const [page, setpage]=useState(1);
+
 
     useEffect(()=>{
-     
-   getGithubuser("albseb")
+     setLoading(true);
+   getGithubuser(query,page)
    .then(res=>{
+       setLoading(false)
     setData(res.data)
+    setError(false)
         })
         .catch(err=>{
+            setLoading(false)
     setError(true)
     console.log(err)
         })
-    },[])
+    },[query,page])
+
+
+    const handleClick =(query)=>setQuery(query)
+    // const handlepageChange= (page)=>setpage(page)
 console.log(data)
     return (
       <div>
             <h1>Github Users</h1>
+            {loading && <div>Loading...</div>}
+            {error && <div>Error...</div>}
+            <SearchBox handleClick={handleClick}/>
         {data?.items?.map((item)=>(<GithubCard key={item.id}{...item}/>))}
+     <div>
+         <button disabled={page===1} onClick={()=>setpage(page-1)}>Prev</button>
+         <button onClick={()=>setpage(page+1)}>Next</button>
+     </div>
       </div>
+       
     );
+    }
+
+    const SearchBox = ({handleClick})=>{
+        const [text,settext] =useState("");
+        return (
+            <div>
+                <input type="text" value={text} onChange={e=>settext(e.target.value)}/>
+                <button onClick={()=>handleClick(text)}>Search</button>
+            </div>
+        )
     }
     const GithubCard=({avatar_url,login})=>{
         return (
